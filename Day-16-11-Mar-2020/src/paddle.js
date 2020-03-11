cc9792ca46e0d20bac0125cdcb7262bc609af602
Yusuf Ali <yali98319@gmail.com>
2020-03-11 22:16:22 +0530

    Added tody's files

---
+++ Day-16-11-Mar-2020/src/paddle.js
@@ -0,0 +1,24 @@
+export default class Paddle {
+
+    constructor(gameWidth, gameHeight) {
+        this.width = 150;
+        
+        this.height = 20;
+
+        this.position = {
+            x : gameWidth / 2 - this.width / 2,
+            y : gameHeight - this.height - 10
+        }
+    }
+    
+    draw(ctx) {
+        ctx.fillStyle = "#26ae60";
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+
+    update(deltaTime) {
+        if(!deltaTime) return;
+
+        this.position.x += 5 / deltaTime;
+    }
+}
