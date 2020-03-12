057c0ae12c4cad577d38f7b118d4298525caf48c
Yusuf Ali <yali98319@gmail.com>
2020-03-12 21:31:41 +0530

    Added code files

---
+++ Day-17-12-Mar-2020/app.js
@@ -0,0 +1,29 @@
+import Paddle from "/src/paddle.js";
+import InputHandler from "/src/input.js";
+
+let canvas = document.getElementById('gamescreen');
+let ctx = canvas.getContext('2d');
+
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
+
+let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+
+new InputHandler(paddle);
+
+paddle.draw(ctx);
+
+let lastTime = 0;
+
+function gameLoop(timestamp) {
+    let deltaTime = timestamp - lastTime;
+    lastTime = timestamp;
+
+    ctx.clearRect(0, 0, 800, 600);
+    paddle.update(deltaTime);
+    paddle.draw(ctx);
+
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
---
+++ Day-17-12-Mar-2020/input.js
@@ -0,0 +1,15 @@
+export default class InputHandler {
+  constructor(paddle) {
+    document.addEventListener('keydown', (event) => {
+      switch (event.keyCode) {
+        case 37:
+          paddle.moveLeft();
+          break;
+
+        case 39:
+          paddle.moveRight();
+          break;
+      }
+    })
+  }
+}
